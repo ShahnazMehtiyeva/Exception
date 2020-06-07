27e3f6a3f93a88acad148d954ed020449cea99f0
Shahnaz436 <47938493+Shahnaz436@users.noreply.github.com>
2020-06-07 12:04:25 +0400

    exception

---
+++ exception/exception/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exception
+{
+    class Program
+    {
+
+        static List<int> list;
+
+        static void FillData(int data)
+        {
+            if (data > 0)
+            {
+                list = new List<int>();
+                list.Add(data);
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    FillData(0);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Sifir ve sifirdan kicik reqemler daxil edile bilmez");
+            //}
+
+            //Console.WriteLine("Hello world");
+
+
+
+            B b = new B();
+            try
+            {
+                C c = (C)b;
+
+                if (b is C)
+                {
+                    Console.WriteLine("Casting");
+                }
+
+                C c2 = b as C;
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+        }
+
+        class A
+        {
+
+        }
+
+        class B : A
+        {
+
+        }
+
+        class C : B
+        {
+
+        }
+    }
+}
